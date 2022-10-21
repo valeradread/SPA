9936b35f12b6e1d9f79bbbedeaedda3856e901a8
valeradread <valera.konechnyj@gmail.com>
2022-10-22 00:01:08 +0300

    refactored userscontainer.jsx. now 'connect' is wrapping our class component and it's render-method returns a clear functional component 'users', wich includes only jsx

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styles from "./Users.module.css";
+import userPhoto from "../../assets/images/no-photo.jpg";
+
+
+let Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount/props.pageSize);
+
+    let pages = [];
+    for(let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+
+    let curP = props.currentPage;
+    let curPF = ((curP - 5) < 0) ?  0  : curP - 5 ;
+    let curPL = curP + 5;
+    let slicedPages = pages.slice( curPF, curPL);
+
+    return <div>
+        <div>
+            {slicedPages.map(p => {
+                return <span className={props.currentPage === p && styles.selectedPage}
+                             onClick={(e) => {
+                                 props.onPageChanged(p);
+                             }}>{p}</span>
+            })}
+        </div>
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
+                    </div>
+                    <div>
+                        {u.followed
+                            ? <button onClick={() => {
+                                props.unfollow(u.id)
+                            }}> Unfollow </button>
+                            : <button onClick={() => {
+                                props.follow(u.id)
+                            }}> Follow </button>}
+
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.name}</div><div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
+                    </span>
+                </span>
+                <p></p>
+            </div>)
+        }
+    </div>
+}
+
+export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-import Users from "./Users";
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../redax/users-reducer";
+import axios from "axios";
+import Users from "./Users";
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setUsersTotalCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageChanged={this.onPageChanged}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -32,4 +61,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
