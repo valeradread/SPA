62a037f80502363566b670a11e512cd60cfb5019
valeradread <valera.konechnyj@gmail.com>
2022-11-09 02:39:06 +0200

    we started to develop UserProfile page

---
+++ src/components/Profile/ProfileContainer.jsx
@@ -0,0 +1,18 @@
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import MyPostsContainer from "./MyPosts/MyPostsContainer";
+
+const Profile = (props) => {
+
+
+
+    return (
+        <div>
+            <ProfileInfo />
+            <MyPostsContainer />
+        </div>
+    );
+}
+
+export default Profile;
